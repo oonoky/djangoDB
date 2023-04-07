4a7258fe39def3613d9f34b5b1445a149cb6d36f
Gulnaz Omirzakova <103216968+oonoky@users.noreply.github.com>
2023-04-07 17:38:24 +0600

    task 7

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor, Genre, Rating
+from .models import Movie, Category, Actor, Genre, Rating, Reviews
 from .forms import ReviewForm, RatingForm
 
 
@@ -31,6 +31,22 @@ class MovieDetailView(GenreYear, DetailView):
         context = super().get_context_data(**kwargs)
         context["star_form"] = RatingForm()
         return context
+
+class AddReview(View):
+    """Отзывы"""
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
+
+        
 
 class AddReview(View):    
     """Отзывы"""
@@ -98,3 +114,15 @@ class JsonFilterMoviesView(ListView):
     def get(self, request, *args, **kwargs):
         queryset = list(self.get_queryset())
         return JsonResponse({"movies": queryset}, safe=False)
+
+class Search(ListView):
+    """Поиск фильмов"""
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get("q"))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["q"] = f'q={self.request.GET.get("q")}&'
+        return context        
